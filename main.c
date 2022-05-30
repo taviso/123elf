ed67cf4f41a89f77120d53899f60470feefe3f25
Jim Huang <jserv@biilabs.io>
2022-05-31 01:46:28 +0800

    Tidy help messages
    After the completion of function 'read_startup_ri_file', a trailing
    newline character was written into the terminal. Therefore, there was
    obviously a blank line between the original 1-2-3 help message and our
    own. Using ANSI escape code, we can move cursor to the beginning of
    the previous line and then dump the desired messages seamlessly.

--- main.c
+++ main.c
@@ -45,6 +45,9 @@ static void print_help()
     if (printed++)
         return;
 
+    // move cursor to the beginning of the previous line
+    printf("\033[F");
+
     printf("        -b                      to enable banner\n");
     printf("        -u                      to disable undo support\n");
 }
