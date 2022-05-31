40a8b1c8b2677bd912f4b151f27b55f11ac95095
Vladimir Vrzić <vvrzic@gmail.com>
2022-05-31 20:58:28 +0200

    More verbose extract.sh; drop bash dependency

--- extract.sh
+++ extract.sh
@@ -1,46 +1,50 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Extract the necessary files from Lotus 1-2-3 UNIX
 #
 
-LOTUS=root/lotus/123.v10
+ROOT="root"
+ORIG="orig"
+LOTUS="${ROOT}/lotus/123.v10"
 
 # Use any tools placed in this directory first.
-export PATH=.:$PATH
+export PATH=".:$PATH"
 
 if ! test -f 123UNIX1.IMG   \
        -a -f 123UNIX2.IMG   \
        -a -f 123UNIX3.IMG   \
        -a -f 123UNIX4.IMG   \
        -a -f 123UNIX5.IMG; then
-    printf "You need to download the original 1-2-3 UNIX IMG files.\n" 1>&2
-    printf "They are available here: https://archive.org/download/123-unix\n" 1>&2
+    echo >&2 'You need to download the original 1-2-3 UNIX IMG files.'
+    echo >&2 'They are available here: https://archive.org/download/123-unix'
     exit 1
 fi
 
-mkdir root
-mkdir orig
+mkdir "${ROOT}"
+mkdir "${ORIG}"
 
-tar -C root -xf 123UNIX1.IMG
+echo "==> Extracting tar archive 123UNIX1.IMG"
+tar -C "${ROOT}" -xf 123UNIX1.IMG
 
-for i in 123UNIX{2..5}.IMG; do
-    cpio -D root -id < ${i}
+for img in 123UNIX2.IMG 123UNIX3.IMG 123UNIX4.IMG 123UNIX5.IMG; do
+    echo "==> Extracting cpio archive $img"
+    cpio -D "${ROOT}" -id < "$img"
 done
 
-find root -iname '*.z' -exec gunzip {} \;
+echo "==> Uncompressing .z files"
+find "${ROOT}" -iname '*.z' -exec gunzip {} \;
 
-cat ${LOTUS}/sysV386/lib/123.o.z_? | gzip -d > orig/123.o
+echo "==> Uncompressing and copying object files"
+cat "${LOTUS}"/sysV386/lib/123.o.z_? | gzip -d > "${ORIG}/123.o"
+cp "${LOTUS}"/sysV386/lib/*.o "${ORIG}/"
 
-cp ${LOTUS}/sysV386/lib/*.o orig/
-
-# Install better keymap
-for i in xterm xterm-256color ; do
-    cp xterm ${LOTUS}/keymaps/${i:0:1}/${i}
+echo "==> Installing better keymaps"
+for t in xterm xterm-256color; do
+    cp -v xterm "${LOTUS}/keymaps/$(echo "${t}" | cut -c1)/${t}"
 done
-
-if ! test -e ${LOTUS}/keymaps/${TERM:0:1}/${TERM}; then
-    cp xterm ${LOTUS}/keymaps/${TERM:0:1}/${TERM}
+if [ ! -f "${LOTUS}/keymaps/$(echo "${TERM}" | cut -c1)/${TERM}" ]; then
+    cp -v xterm "${LOTUS}/keymaps/$(echo "${TERM}" | cut -c1)/${TERM}"
 fi
 
-# Copy the banner template over
-cp root/usr/tmp/lotus_install/123/banner ${LOTUS}/ri/USA-English/123ban.ri
+echo "==> Copying the banner template over"
+cp -v "${ROOT}/usr/tmp/lotus_install/123/banner" "${LOTUS}/ri/USA-English/123ban.ri"
