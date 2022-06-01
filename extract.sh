8a9f771b84de164b234e99058d8b9ac9cb66d3f2
Vladimir Vrzić <vvrzic@gmail.com>
2022-06-01 23:16:29 +0200

    More extract improvements:
    - use POSIX options with cpio and tar for portability
    - drop dependency on working directory
    - add 'clean' subprogram

--- extract.sh
+++ extract.sh
@@ -2,19 +2,25 @@
 #
 # Extract the necessary files from Lotus 1-2-3 UNIX
 #
-
-ROOT="root"
-ORIG="orig"
+BASE="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+IMG="${BASE}"
+ROOT="${BASE}/root"
+ORIG="${BASE}/orig"
 LOTUS="${ROOT}/lotus/123.v10"
 
-# Use any tools placed in this directory first.
-export PATH=".:$PATH"
+if [ "$1" = 'clean' ]; then
+    rm -rfv "${ROOT}" "${ORIG}"
+    exit
+fi
 
-if ! test -f 123UNIX1.IMG   \
-       -a -f 123UNIX2.IMG   \
-       -a -f 123UNIX3.IMG   \
-       -a -f 123UNIX4.IMG   \
-       -a -f 123UNIX5.IMG; then
+# Use any tools placed in this directory first.
+export PATH="${BASE}:$PATH"
+
+if ! test -f "${IMG}/123UNIX1.IMG"   \
+       -a -f "${IMG}/123UNIX2.IMG"   \
+       -a -f "${IMG}/123UNIX3.IMG"   \
+       -a -f "${IMG}/123UNIX4.IMG"   \
+       -a -f "${IMG}/123UNIX5.IMG"; then
     echo >&2 'You need to download the original 1-2-3 UNIX IMG files.'
     echo >&2 'They are available here: https://archive.org/download/123-unix'
     exit 1
@@ -24,12 +30,16 @@ mkdir "${ROOT}"
 mkdir "${ORIG}"
 
 echo "==> Extracting tar archive 123UNIX1.IMG"
-tar -C "${ROOT}" -xf 123UNIX1.IMG
+cd "${ROOT}"
+tar xf "${IMG}/123UNIX1.IMG"
+cd -
 
-for img in 123UNIX2.IMG 123UNIX3.IMG 123UNIX4.IMG 123UNIX5.IMG; do
+cd "${ROOT}"
+for img in "${IMG}/123UNIX2.IMG" "${IMG}/123UNIX3.IMG" "${IMG}/123UNIX4.IMG" "${IMG}/123UNIX5.IMG"; do
     echo "==> Extracting cpio archive $img"
-    cpio -D "${ROOT}" -id < "$img"
+    cpio -id < "$img"
 done
+cd -
 
 echo "==> Uncompressing .z files"
 find "${ROOT}" -iname '*.z' -exec gunzip {} \;
@@ -40,10 +50,10 @@ cp "${LOTUS}"/sysV386/lib/*.o "${ORIG}/"
 
 echo "==> Installing better keymaps"
 for t in xterm xterm-256color; do
-    cp -v xterm "${LOTUS}/keymaps/$(echo "${t}" | cut -c1)/${t}"
+    cp -v "${BASE}/xterm" "${LOTUS}/keymaps/$(echo "${t}" | cut -c1)/${t}"
 done
 if [ ! -f "${LOTUS}/keymaps/$(echo "${TERM}" | cut -c1)/${TERM}" ]; then
-    cp -v xterm "${LOTUS}/keymaps/$(echo "${TERM}" | cut -c1)/${TERM}"
+    cp -v "${BASE}/xterm" "${LOTUS}/keymaps/$(echo "${TERM}" | cut -c1)/${TERM}"
 fi
 
 echo "==> Copying the banner template over"
