6d4db4a9f3df7c8c21c5b6e1518e2946613de5ec
Tavis Ormandy <taviso@gmail.com>
2022-06-02 17:24:57 -0700

    First attempt at keymap generator for #65
    This tool will generate a keymap file for your current $TERM,
    you use it like this:
    
    $ ./keymap > $TERM
    
    Then save the keymap file to the right directory in the lotus root (e.g.
    root/lotus/123.v10/keymaps/{first letter}/{termname})

---
+++ keymap/keymap.c
@@ -0,0 +1,525 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <curses.h>
+#include <term.h>
+#include <assert.h>
+#include <err.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "keymap.h"
+
+// It is safe to rearrange and change this array.
+//  - name is just a human readable description of the key sequence,
+//    like "Ctrl+PgUp", it is not used by 1-2-3.
+//  - cap is a terminfo capability, you can look them up in terminfo(5).
+//  - kfun is a 1-2-3 function id you want this mapped to, like {HOME}.
+//
+// You can have multiple sequences mapped to the same kfun, i.e. you can have
+// Alt+F8 and Alt+F9 both be {HELP}.
+// You cannot have multiple kfuns mapped to the same key sequence, i.e.
+// Alt+F8 cannot be {HELP} and {UNDO}.
+static KEYDEF keydefs[] = {
+    { "Home",               "khome",    KFUN_HOME },
+    { "End",                "kend",     KFUN_END },
+    { "Up Arrow",           "kcuu1",    KFUN_UP },
+    { "Down Arrow",         "kcud1",    KFUN_DOWN },
+    { "Left Arrow",         "kcub1",    KFUN_LEFT },
+    { "Right Arrow",        "kcuf1",    KFUN_RIGHT },
+    { "Pg Up",              "kpp",      KFUN_PG_UP },
+    { "Pg Dn",              "knp",      KFUN_PG_DN },
+    { "Ctrl Left Arrow",    "kLFT5",    KFUN_BIG_LEFT },
+    { "Ctrl Right Arrow",   "kRIT5",    KFUN_BIG_RIGHT },
+    { "Ctrl Pg Up",         "kPRV5",    KFUN_NEXT_SHEET },
+    { "Ctrl Pg Dn",         "kNXT5",    KFUN_PREV_SHEET },
+    { "Ctrl Home",          "kHOM5",    KFUN_FIRST_CELL },
+    { "Ctrl End",           "kEND5",    KFUN_FILE },
+    { "Tab",                "ht",       KFUN_TAB },
+    { "Backspace",          "kbs",      KFUN_BACKSPACE },
+    { "Enter",              "kent",     KFUN_RETURN },
+    { "Del",                "kdch1",    KFUN_DEL },
+    { "Ins",                "kich1",    KFUN_INS },
+    { "F12",                "kf12",     KFUN_SCROLL_LOCK },
+    { "F1",                 "kf1",      KFUN_HELP },
+    { "F2",                 "kf2",      KFUN_EDIT },
+    { "F3",                 "kf3",      KFUN_NAME },
+    { "F4",                 "kf4",      KFUN_ABS },
+    { "F5",                 "kf5",      KFUN_GOTO },
+    { "F6",                 "kf6",      KFUN_WINDOW },
+    { "F7",                 "kf7",      KFUN_QUERY },
+    { "F8",                 "kf8",      KFUN_TABLE },
+    { "F9",                 "kf9",      KFUN_CALC },
+    { "F10",                "kf10",     KFUN_GRAPH },
+    { "Alt-F1",             "kf49",     KFUN_COMPOSE },
+    { "Alt-F2",             "kf50",     KFUN_RECORD },
+    { "Alt-F3",             "kf51",     KFUN_RUN },
+    { "Alt-F4",             "kf52",     KFUN_UNDO },
+    { "Alt-F5",             "kf53",     KFUN_UNDO }, // Alternative {UNDO}
+    { "Alt-F6",             "kf54",     KFUN_ZOOM },
+};
+
+// This adds room for a new node and returns the new node offset. The size
+// pointer would normally be &header.keytreesize. If you had a pointer you
+// were using to navigate the tree, pass it as state to have it adjusted
+// (in case realloc moves it).
+static int key_new_node(PKEYINFO *base, PKEYINFO *state, uint32_t *keytreesize)
+{
+    PKEYINFO newnode;
+    int newoff;
+    int stateidx;
+
+    // Optionally fixup a cursor to handle a moved base.
+    if (state) stateidx = keyidx(*base, *state);
+
+    *base = realloc(*base, *keytreesize + sizeof(KEYINFO));
+
+    // Find out where the new node is so we can zero it.
+    newnode = (PKEYINFO)((char *)(*base) + *keytreesize);
+
+    // Initialize the new node.
+    newoff = keyoff(*base, memset(newnode, 0, sizeof *newnode));
+
+    // Record the new size.
+    *keytreesize += sizeof **base;
+
+    // Fixup the cursor pointer.
+    if (state) *state = &(*base)[stateidx];
+
+    // Return the new index.
+    return newoff;
+}
+
+// This is used to sort the definitions array to try to compact the
+// tree.
+// FIXME: I don't really know what the optimal way to sort it is yet.
+static int compare_keyseq(const void *a, const void *b)
+{
+    const struct KEYDEF *x = a;
+    const struct KEYDEF *y = b;
+
+    // Put undefined sequences at the end.
+    if (x->kseq == NULL && y->kseq == NULL)
+        return 0;
+    if (x->kseq == NULL)
+        return -1;
+    if (y->kseq == NULL)
+        return 1;
+
+    return strcmp(x->kseq, y->kseq);
+}
+
+
+static bool append_key_sequence(PKEYINFO *base,
+                                PKEYMAP hdr,
+                                const char *keyseq,
+                                uint16_t kfun)
+{
+    if (!keyseq || *keyseq == '\0') {
+        errx(EXIT_FAILURE, "You cannot have an empty key sequence");
+    }
+
+    if (!base || !kfun) {
+        errx(EXIT_FAILURE, "The parameters were invalid.");
+    }
+
+    // If this is an empty base we can allocate a new one.
+    if (*base == NULL) {
+        key_new_node(base, NULL, &hdr->keytreesize);
+    }
+
+    // We start at root, and see how far we can get in the existing
+    // tree.
+    for (PKEYINFO root = *base; root;) {
+        // This means wait for a timeout before continuing.
+        if (root->keyval == KEY_TIMEOUT) {
+            // This is not the end, and there's a next pointer.
+            if (*keyseq && root->data.next) {
+                root = keyptr(*base, root->data.next);
+                continue;
+            }
+            // This is not the end, and theres no next pointer.
+            if (*keyseq && !root->data.next) {
+                // We need to add the next pointer.
+                int nextoff = key_new_node(base, &root, &hdr->keytreesize);
+                root->data.next = nextoff;
+
+                // Now switch to the new node.
+                root = keyptr(*base, nextoff);
+                continue;
+            }
+            // We are at the end, so we want to take this node.
+            if (root->kfun && root->kfun != kfun) {
+                errx(EXIT_FAILURE, "Duplicate key sequences?");
+            }
+            if (root->kfun && root->kfun == kfun) {
+                errx(EXIT_FAILURE, "Adding the same sequence twice?");
+            }
+
+            // Okay, this node belongs to us now.
+            root->kfun = kfun;
+            return true;
+        }
+
+        // This is an empty node that needs to be filled.
+        if (root->keyval == 0) {
+            root->keyval = *keyseq;
+        }
+
+        // This is the next node in our key sequence.
+        if (*keyseq == root->keyval) {
+            // Advance sequence to next character. If this is nul
+            // then thats the end of the sequence.
+            ++keyseq;
+
+            // If this node already has a kfun, then that means
+            // another sequence shares this prefix. We need to inject
+            // a timeout sequence.
+            if (root->kfun) {
+                int nextoff = key_new_node(base, &root, &hdr->keytreesize);
+
+                // Create the new node.
+                keyptr(*base, nextoff)->keyval       = KEY_TIMEOUT;
+                keyptr(*base, nextoff)->kfun         = root->kfun;
+                keyptr(*base, nextoff)->data.timeout = 1;
+
+                // Now make sure this node makes sense.
+                root->kfun = 0;
+                root->next = nextoff;
+
+                // Move to the next node.
+                root            = keyptr(*base, nextoff);
+                continue;
+            }
+
+            // This seems like a pointer to a timeout.
+            if (root->kfun == 0 && root->next) {
+                root = keyptr(*base, root->next);
+                continue;
+            }
+
+            // It seems like nobody else wants this, so we can claim it.
+            if (root->kfun == 0 && !root->next && !*keyseq) {
+                root->kfun = kfun;
+                return true;
+            }
+
+            if (root->kfun == 0 && !root->next && *keyseq) {
+                int nextoff = key_new_node(base, &root, &hdr->keytreesize);
+                root->next = nextoff;
+                root = keyptr(*base, nextoff);
+                continue;
+            }
+
+            // Not reached.
+            assert(0);
+        }
+
+        // This is not the right node, we do a binary search for the correct
+        // one.
+        if (*keyseq > root->keyval) {
+            if (root->node.nextkey_hi == 0) {
+                int nextoff = key_new_node(base, &root, &hdr->keytreesize);
+                root->node.nextkey_hi = nextoff;
+            }
+            root = keyptr(*base, root->node.nextkey_hi);
+            continue;
+        }
+
+        if (*keyseq < root->keyval) {
+            if (root->node.nextkey_lo == 0) {
+                int nextoff = key_new_node(base, &root, &hdr->keytreesize);
+                root->node.nextkey_lo = nextoff;
+            }
+            root = keyptr(*base, root->node.nextkey_lo);
+            continue;
+        }
+
+        // How did I get here?
+        abort();
+    }
+
+    errx(EXIT_FAILURE, "failed to construct keytree");
+}
+
+int main(int argc, char **argv)
+{
+    struct KEYINFO *keys = NULL;
+    uint32_t numkeys;
+    uint32_t count;
+    uint32_t ptr;
+    uint16_t *keyfuncs;
+    uint16_t match;
+    uint32_t index;
+    char **keynames;
+    char **keyseqs;
+    struct KEYMAP hdr = {
+        .magic = 0x101,
+        .version = 1,
+        .zero = 0,
+        .keytreesize = 0,
+        .keydatasize = 0,
+        .root = sizeof hdr,
+    };
+
+    // Let ncurses know what our terminal is.
+    if (setupterm(NULL, STDIN_FILENO, NULL) != OK) {
+        err(EXIT_FAILURE, "setupterm failed, is $TERM correct?");
+    }
+
+    // We can safely use more capabilities.
+    use_extended_names(true);
+
+    if (isatty(STDOUT_FILENO)) {
+        errx(EXIT_FAILURE, "Please redirect stdout to a file, e.g. %s > %s",
+                           argv[0],
+                           getenv("TERM"));
+    }
+
+    // Count how many key definitions there are.
+    numkeys = sizeof(keydefs) / sizeof(struct KEYDEF);
+
+    // First we need to lookup each of the keys.
+    for (int i = 0; i < numkeys; i++) {
+        char *sequence = tigetstr(keydefs[i].cap);
+
+        // Keep track of how much data we need to append.
+        hdr.keydatasize += strlen(keydefs[i].name);
+        hdr.keydatasize += strlen(keydefs[i].cap);
+        hdr.keydatasize += 2;
+
+        // FIXME: Is this right??  I'm trying to get the normal mode
+        // sequence, and terminfo gives me the application mode sequence.
+        // FIXME: I have no idea how this works.
+        //if (sequence[0] == '\033' && sequence[1] == 'O' && sequence[2] != '\0') {
+        //    sequence[1] = '[';
+        //}
+
+        // Check that ncurses recognizes this capability.
+        if (sequence == NULL || sequence == (char *) ERR) {
+            warnx("you do not appear to have a %s key", keydefs[i].name);
+            continue;
+        }
+
+        // Seems okay, add it to the array.
+        keydefs[i].kseq = sequence;
+    }
+
+    // Now we sort the array so that the generated tree is compact.
+    qsort(keydefs,
+          numkeys,
+          sizeof(struct KEYDEF),
+          compare_keyseq);
+
+    // Now we build our tree.
+    // First, add the sequences that are the same for everyone.
+    append_key_sequence(&keys, &hdr, "\e", KFUN_ESC);
+    append_key_sequence(&keys, &hdr, "\f", KFUN_REFRESH);
+    append_key_sequence(&keys, &hdr, "\b", KFUN_BACKSPACE);
+
+    // Now append all the shortcuts.
+    for (int i = 0; i < numkeys; i++) {
+        append_key_sequence(&keys, &hdr, keydefs[i].kseq, keydefs[i].kfun);
+    }
+
+    // Now the macro shortcut keys, Alt-A - Alt-Z
+    for (int c = 0; c < 26; c++) {
+        char seqlower[3] = { '\033', 'a' + c, '\0' };
+        char sequpper[3] = { '\033', 'A' + c, '\0' };
+        append_key_sequence(&keys, &hdr, seqlower, KFUN_MAC_A + c);
+        append_key_sequence(&keys, &hdr, sequpper, KFUN_MAC_A + c);
+    }
+
+    // Write the header and key tree out.
+    fwrite(&hdr, sizeof hdr, 1, stdout);
+    fwrite(keys, hdr.keytreesize, 1, stdout);
+
+    // As far as I know, lotus never looks at the data but I'll append it anyway.
+    fwrite(&numkeys, sizeof numkeys, 1, stdout);
+
+    for (int i = 0; i < hdr.keytreesize / sizeof(KEYINFO); i++) {
+        static uint16_t dummy;
+
+        if (i < numkeys) {
+            fwrite(&keydefs[i].kfun, sizeof keydefs[i].kfun, 1, stdout);
+            continue;
+        }
+
+        // I think it expects data, even if we dont use it.
+        fwrite(&dummy, sizeof dummy, 1, stdout);
+    }
+
+    // Write out names.
+    for (int i = 0; i < numkeys; i++) {
+        fwrite(keydefs[i].name, strlen(keydefs[i].name) + 1, 1, stdout);
+    }
+
+    // Write out sequences.
+    for (int i = 0; i < numkeys; i++) {
+        fwrite(keydefs[i].kseq, strlen(keydefs[i].kseq) + 1, 1, stdout);
+    }
+
+    // Done.
+    return 0;
+}
+
+// Just for debugging, parse a keymap on stdin and try to lookup sequence.
+#if 0
+static int parse_keymap(const char *sequence)
+{
+    struct KEYMAP hdr;
+    struct KEYINFO *keys;
+    uint32_t numkeys;
+    uint32_t count;
+    uint32_t ptr;
+    uint16_t *keyfuncs;
+    uint16_t match;
+    uint32_t index;
+    char **keynames;
+    char **keyseqs;
+
+    if (fread(&hdr, sizeof hdr, 1, stdin) != 1) {
+        err(EXIT_FAILURE, "failed to read in keymap header");
+    }
+
+    fprintf(stdout, "hdr.magic          %04X\n", hdr.magic);
+    fprintf(stdout, "hdr.version        %04X\n", hdr.version);
+    fprintf(stdout, "hdr.zero           %08X\n", hdr.zero);
+    fprintf(stdout, "hdr.keytreesize    %08X\n", hdr.keytreesize);
+    fprintf(stdout, "hdr.keydatasize    %08X\n", hdr.keydatasize);
+    fprintf(stdout, "hdr.root           %08X\n", hdr.root);
+
+    if (hdr.magic != 0x101) {
+        errx(EXIT_FAILURE, "this does not seem to be a 123 keymap");
+    }
+
+    if (hdr.version != 1) {
+        errx(EXIT_FAILURE, "this version %u is not supported", hdr.version);
+    }
+
+    count = hdr.keytreesize / sizeof(*keys);
+
+    if (hdr.keytreesize % sizeof(*keys)) {
+        errx(EXIT_FAILURE, "keytreesize does not seem right");
+    }
+
+    keys = calloc(count, sizeof *keys);
+
+    if (fread(keys, sizeof *keys, count, stdin) != count) {
+        err(EXIT_FAILURE, "failed to read in the key tree");
+    }
+
+    if (fread(&numkeys, sizeof numkeys, 1, stdin) != 1) {
+        err(EXIT_FAILURE, "failed to read key table size");
+    }
+
+    keyfuncs = calloc(count, sizeof *keyfuncs);
+    keynames = calloc(numkeys, sizeof *keynames);
+    keyseqs  = calloc(numkeys, sizeof *keyseqs);
+
+    if (fread(keyfuncs, sizeof *keyfuncs, count, stdin) != count) {
+        err(EXIT_FAILURE, "failed to read in key function codes");
+    }
+
+    // Now we read names and sequences.
+    for (int i = 0; i < numkeys; i++) {
+        char keystr[256] = {0};
+
+        // Read in a nul terminated string.
+        for (int j = 0; j < sizeof(keystr) - 1; j++) {
+            if ((keystr[j] = fgetc(stdin)) <= 0)
+                break;
+        }
+
+        keynames[i] = strdup(keystr);
+    }
+
+    for (int i = 0; i < numkeys; i++) {
+        char keystr[256] = {0};
+
+        // Read in a nul terminated string.
+        for (int j = 0; j < sizeof(keystr) - 1; j++) {
+            if ((keystr[j] = fgetc(stdin)) <= 0)
+                break;
+        }
+
+        keyseqs[i] = strdup(keystr);
+    }
+
+    if (fgetc(stdin) != EOF) {
+        fprintf(stderr, "not at eof after sequence table!\n");
+    }
+
+    match = lookup_key_sequence(argv[1],
+                                keys,
+                                keyptr(keys, hdr.root));
+
+    // Find this match in the table.
+    for (index = 0; index < numkeys; index++) {
+        if (keyfuncs[index] == match)
+            break;
+    }
+
+    printf("sequence '%s' is %s, mapped to function 123 %u (%s)\n",
+           keyseqs[index],
+           keynames[index],
+           match,
+           kfun_name(match));
+
+    return 0;
+}
+
+// Try to figure out what kfun the sequence specified is mapped to.
+uint16_t lookup_key_sequence(const char *sequence,
+                             struct KEYINFO *base,
+                             struct KEYINFO *root)
+{
+    if (!sequence || !*sequence || !base || !root) {
+        warn("Invalid parameters passed to lookup_key_sequence");
+        return 0;
+    }
+
+    while (root) {
+        // This means wait for a timeout before continuing.
+        if (root->keyval == KEY_TIMEOUT) {
+            // For our purposes, the timeout did not expire.
+            root = keyptr(base, root->data.next);
+            continue;
+        }
+
+        // This is the next node in our sequence.
+        if (*sequence == root->keyval) {
+            // If this is the end of our sequence and kfun is set
+            // use this. If kfun is not set, there is a custom timeout, which
+            // we ignore as we're just looking up a sequence.
+            if (!*++sequence && root->kfun) {
+                return root->kfun;
+            }
+
+            // Continue to next in chain.
+            root = keyptr(base, root->next);
+            continue;
+        }
+
+        // Now we do a binary search for the next key in our sequence.
+        if (*sequence > root->keyval) {
+            root = keyptr(base, root->node.nextkey_hi);
+            continue;
+        }
+
+        if (*sequence < root->keyval) {
+            root = keyptr(base, root->node.nextkey_lo);
+            continue;
+        }
+
+        // How did I get here?
+        abort();
+    }
+
+    warn("The key sequence specified was not defined");
+    return 0;
+}
+
+#endif
---
+++ keymap/keymap.h
@@ -0,0 +1,220 @@
+#ifndef __KEYMAP_H
+#define __KEYMAP_H
+
+#pragma pack(1)
+
+typedef struct KEYINFO {
+    uint16_t    keyval;
+    uint16_t    kfun;
+    union {
+        struct KEYNODE {
+            uint32_t nextkey_hi;
+            uint32_t nextkey_lo;
+        } node;
+        struct KEYDATA {
+            uint32_t next;
+            uint32_t timeout;
+        } data;
+    };
+    uint32_t next;
+} KEYINFO, *PKEYINFO;
+
+typedef struct KEYMAP {
+    uint16_t magic;
+    uint16_t version;
+    uint32_t zero;
+    uint32_t keytreesize;
+    uint32_t keydatasize;
+    uint32_t root;
+} KEYMAP, *PKEYMAP;
+
+typedef struct KEYDEF {
+    const char *name;
+    const char *cap;
+    uint16_t kfun;
+    const char *kseq;
+} KEYDEF, *PKEYDEF;
+
+// The offsets in the KEYINFO tree are encoded in an unusual way, these
+// inline functions abstract away the ugly casting required to use them
+// when traversing the tree.
+//
+// Given the base of a keytree and an offset, give me a new node pointer.
+static inline PKEYINFO keyptr(const PKEYINFO base, uint32_t offset)
+{
+    if (offset == 0)
+        return NULL;
+
+    return (PKEYINFO)((uintptr_t)(base) + offset - sizeof(KEYMAP));
+}
+// Given the base of a keytree and an entry, return the array index.
+static inline int keyidx(const PKEYINFO base, const PKEYINFO state)
+{
+    return ((uintptr_t)(state) - (uintptr_t)(base)) / sizeof (KEYINFO);
+}
+// Given the base of a keytree and an entry, return an offset suitable for
+// storing as node ptr.
+static inline int keyoff(const PKEYINFO base, const PKEYINFO state)
+{
+    return sizeof(KEYMAP) + (uintptr_t)(state) - (uintptr_t)(base);
+}
+
+enum {
+    KFUN_HOME = 0x1801,
+    KFUN_FIRST_CELL = 0x1802,
+    KFUN_END = 0x1803,
+    KFUN_UP = 0x1804,
+    KFUN_DOWN = 0x1805,
+    KFUN_LEFT = 0x1806,
+    KFUN_RIGHT = 0x1807,
+    KFUN_PG_UP = 0x1808,
+    KFUN_PG_DN = 0x1809,
+    KFUN_BIG_LEFT = 0x180a,
+    KFUN_BIG_RIGHT = 0x180b,
+    KFUN_TAB = 0x180c,
+    KFUN_WINDOW = 0x180d,
+    KFUN_NEXT_SHEET = 0x180e,
+    KFUN_PREV_SHEET = 0x180f,
+    KFUN_BREAK = 0x1810,
+    KFUN_ESC = 0x1811,
+    KFUN_RETURN = 0x1812,
+    KFUN_INS = 0x1813,
+    KFUN_DEL = 0x1814,
+    KFUN_BACKSPACE = 0x1815,
+    KFUN_HELP = 0x1816,
+    KFUN_ABS = 0x1817,
+    KFUN_GOTO = 0x1818,
+    KFUN_CALC = 0x1819,
+    KFUN_EDIT = 0x181a,
+    KFUN_NAME = 0x181b,
+    KFUN_ZOOM = 0x181c,
+    KFUN_GRAPH = 0x181d,
+    KFUN_TABLE = 0x181e,
+    KFUN_QUERY = 0x181f,
+    KFUN_UNDO = 0x1820,
+    KFUN_COMPOSE = 0x1821,
+    KFUN_FILE = 0x1822,
+    KFUN_HAL = 0x1823,
+    KFUN_ADDIN = 0x1824,
+    KFUN_APP1 = 0x1825,
+    KFUN_APP2 = 0x1826,
+    KFUN_APP3 = 0x1827,
+    KFUN_MAC_A = 0x1828,
+    KFUN_MAC_B = 0x1829,
+    KFUN_MAC_C = 0x182a,
+    KFUN_MAC_D = 0x182b,
+    KFUN_MAC_E = 0x182c,
+    KFUN_MAC_F = 0x182d,
+    KFUN_MAC_G = 0x182e,
+    KFUN_MAC_H = 0x182f,
+    KFUN_MAC_I = 0x1830,
+    KFUN_MAC_J = 0x1831,
+    KFUN_MAC_K = 0x1832,
+    KFUN_MAC_L = 0x1833,
+    KFUN_MAC_M = 0x1834,
+    KFUN_MAC_N = 0x1835,
+    KFUN_MAC_O = 0x1836,
+    KFUN_MAC_P = 0x1837,
+    KFUN_MAC_Q = 0x1838,
+    KFUN_MAC_R = 0x1839,
+    KFUN_MAC_S = 0x183a,
+    KFUN_MAC_T = 0x183b,
+    KFUN_MAC_U = 0x183c,
+    KFUN_MAC_V = 0x183D,
+    KFUN_MAC_W = 0x183e,
+    KFUN_MAC_X = 0x183f,
+    KFUN_MAC_Y = 0x1840,
+    KFUN_MAC_Z = 0x1841,
+    KFUN_RUN = 0x1842,
+    KFUN_STEP = 0x1843,
+    KFUN_RECORD = 0x1844,
+    KFUN_FLUSH = 0x1845,
+    KFUN_BELL = 0x1846,
+    KFUN_SCROLL_LOCK = 0x184e,
+    KFUN_REFRESH = 0x1864,
+};
+
+static const char * kfun_name(uint16_t kfun)
+{
+    switch (kfun) {
+        case KFUN_HOME: return "{HOME}";
+        case KFUN_FIRST_CELL: return "{FIRST CELL}";
+        case KFUN_END: return "{END}";
+        case KFUN_UP: return "{UP}";
+        case KFUN_DOWN: return "{DOWN}";
+        case KFUN_LEFT: return "{LEFT}";
+        case KFUN_RIGHT: return "{RIGHT}";
+        case KFUN_PG_UP: return "{PG UP}";
+        case KFUN_PG_DN: return "{PG DN}";
+        case KFUN_BIG_LEFT: return "{BIG LEFT}";
+        case KFUN_BIG_RIGHT: return "{BIG RIGHT}";
+        case KFUN_TAB: return "{TAB}";
+        case KFUN_WINDOW: return "{WINDOW}";
+        case KFUN_NEXT_SHEET: return "{NEXT SHEET}";
+        case KFUN_PREV_SHEET: return "{PREV SHEET}";
+        case KFUN_BREAK: return "{BREAK}";
+        case KFUN_ESC: return "{ESC}";
+        case KFUN_RETURN: return "{RETURN}";
+        case KFUN_INS: return "{INS}";
+        case KFUN_DEL: return "{DEL}";
+        case KFUN_BACKSPACE: return "{BACKSPACE}";
+        case KFUN_HELP: return "{HELP}";
+        case KFUN_ABS: return "{ABS}";
+        case KFUN_GOTO: return "{GOTO}";
+        case KFUN_CALC: return "{CALC}";
+        case KFUN_EDIT: return "{EDIT}";
+        case KFUN_NAME: return "{NAME}";
+        case KFUN_ZOOM: return "{ZOOM}";
+        case KFUN_GRAPH: return "{GRAPH}";
+        case KFUN_TABLE: return "{TABLE}";
+        case KFUN_QUERY: return "{QUERY}";
+        case KFUN_UNDO: return "{UNDO}";
+        case KFUN_COMPOSE: return "{COMPOSE}";
+        case KFUN_FILE: return "{FILE}";
+        case KFUN_HAL: return "{HAL}";
+        case KFUN_ADDIN: return "{ADDIN}";
+        case KFUN_APP1: return "{APP1}";
+        case KFUN_APP2: return "{APP2}";
+        case KFUN_APP3: return "{APP3}";
+        case KFUN_MAC_A: return "{MAC_A}";
+        case KFUN_MAC_B: return "{MAC_B}";
+        case KFUN_MAC_C: return "{MAC_C}";
+        case KFUN_MAC_D: return "{MAC_D}";
+        case KFUN_MAC_E: return "{MAC_E}";
+        case KFUN_MAC_F: return "{MAC_F}";
+        case KFUN_MAC_G: return "{MAC_G}";
+        case KFUN_MAC_H: return "{MAC_H}";
+        case KFUN_MAC_I: return "{MAC_I}";
+        case KFUN_MAC_J: return "{MAC_J}";
+        case KFUN_MAC_K: return "{MAC_K}";
+        case KFUN_MAC_L: return "{MAC_L}";
+        case KFUN_MAC_M: return "{MAC_M}";
+        case KFUN_MAC_N: return "{MAC_N}";
+        case KFUN_MAC_O: return "{MAC_O}";
+        case KFUN_MAC_P: return "{MAC_P}";
+        case KFUN_MAC_Q: return "{MAC_Q}";
+        case KFUN_MAC_R: return "{MAC_R}";
+        case KFUN_MAC_S: return "{MAC_S}";
+        case KFUN_MAC_T: return "{MAC_T}";
+        case KFUN_MAC_U: return "{MAC_U}";
+        case KFUN_MAC_V: return "{MAC_V}";
+        case KFUN_MAC_W: return "{MAC_W}";
+        case KFUN_MAC_X: return "{MAC_X}";
+        case KFUN_MAC_Y: return "{MAC_Y}";
+        case KFUN_MAC_Z: return "{MAC_Z}";
+        case KFUN_RUN: return "{RUN}";
+        case KFUN_STEP: return "{STEP}";
+        case KFUN_RECORD: return "{RECORD}";
+        case KFUN_FLUSH: return "{FLUSH}";
+        case KFUN_BELL: return "{BELL}";
+        case KFUN_SCROLL_LOCK: return "{SCROLL LOCK}";
+        case KFUN_REFRESH: return "{REFRESH}";
+
+    }
+
+    return NULL;
+}
+
+#define KEY_TIMEOUT 0x100
+
+#endif
