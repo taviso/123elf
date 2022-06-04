3597111ff2d6f1a58703364ff1b5b1bd218c03fe
Vladimir Vrzić <vvrzic@gmail.com>
2022-06-04 08:39:13 +0200

    Shell script portability improvements
    - support BSD tar(1)
    - support BSD install(1)
    - remove dependency on working directory from binutils and gzip build scripts
    - explicit dependency on GNU make (gmake(1))

--- gzip.sh
+++ gzip.sh
@@ -6,41 +6,41 @@ GZIP_DL="https://ftp.gnu.org/gnu/gzip"
 GZIP_XZ="gzip-1.12.tar.xz"
 GZIP_DIR="$(basename $GZIP_XZ .tar.xz)"
 GZIP_URL="$GZIP_DL/$GZIP_XZ"
-ORIG_DIR="$(pwd)"
+ORIGIN_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 # Dont continue on error.
 set -e
 
 # Help text.
 if [ "$1" = '-h' ]; then
-   echo >&2 "Usage: $0 [clean]"
-   echo >&2
-   echo >&2 'Specify "clean" to remove gzip, otherwise it will be downloaded and built.'
-   exit 1
+    echo >&2 "Usage: $0 [clean]"
+    echo >&2
+    echo >&2 'Specify "clean" to remove gzip, otherwise it will be downloaded and built.'
+    exit 1
 fi
 
 # Optional cleanup if requested.
 if [ "$1" = 'clean' ]; then
-   rm -fv gzip gunzip zcat "$GZIP_XZ"
-   rm -rfv "$GZIP_DIR"
-   exit
+    rm -fv "$ORIGIN_DIR/gzip" "$ORIGIN_DIR/gunzip" "$ORIGIN_DIR/zcat"
+    rm -rfv "$GZIP_DIR"
+    rm -fv "$ORIGIN_DIR/$GZIP_XZ"
+    exit
 fi
 
 # Download gzip.
 if [ ! -f "$GZIP_XZ" ]; then
-   wget "$GZIP_URL"
+    wget "$GZIP_URL"
 fi
 
 # Extract gzip.
 if [ ! -d "$GZIP_DIR" ]; then
-   tar xf "$GZIP_XZ"
+    xzcat "$GZIP_XZ" | tar xf -
 fi
 
 # Compile gzip.
 if [ ! -x "$GZIP_DIR/gzip" ]; then
-   cd "$GZIP_DIR"
-   ./configure
-   make -j$(nproc)
+   cd "$GZIP_DIR" && ./configure && gmake -j$(nproc)
+   cd "$ORIGIN_DIR"
 fi
 
 # Copy compiled binaries to working directory.
@@ -48,7 +48,6 @@ copy() {
    test ! -x "$2" && cp -v "$1" "$2"
 }
 
-cd "$ORIG_DIR"
-copy "$GZIP_DIR/gzip" gzip
-copy "$GZIP_DIR/gunzip" gunzip
-copy "$GZIP_DIR/zcat" zcat
+copy "$GZIP_DIR/gzip" "$ORIGIN_DIR/gzip"
+copy "$GZIP_DIR/gunzip" "$ORIGIN_DIR/gunzip"
+copy "$GZIP_DIR/zcat" "$ORIGIN_DIR/zcat"
