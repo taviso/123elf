8a040b3d276487f02d6537cec4e9d4d96cbdb349
Vladimir Vrzić <vvrzic@gmail.com>
2022-06-05 09:14:11 +0200

    Better ncurses linker flags detection

--- ncurses-config.sh
+++ ncurses-config.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 nclibs="-lncurses"
 if command -v ncurses6-config > /dev/null; then
-    nclibs="$(ncurses6-config --libs-only-l)"
+    nclibs="$(ncurses6-config --libs)"
 elif command -v pkg-config > /dev/null; then
-    pkgcfglibs="$(pkg-config --libs ncurses)"
-    if [ -n "$pkgcfglibs" ]; then
+    pkgcfglibs="$(pkg-config --libs ncurses 2>/dev/null)"
+    if [ "$?" -eq 0 -a -n "$pkgcfglibs" ]; then
         nclibs="$pkgcfglibs"
     fi
 fi
