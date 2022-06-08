98c3acef14ade6e85b39354484f9489f4efb792d
Vladimir Vrzić <vvrzic@gmail.com>
2022-06-08 12:50:57 +0200

    Find GNU make in PATH

--- binutils.sh
+++ binutils.sh
@@ -6,8 +6,16 @@ BINUTILS_DIR="$(basename $BINUTILS_XZ .tar.xz)"
 BINUTILS_URL="$BINUTILS_DL/$BINUTILS_XZ"
 ORIGIN_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
-# Dont continue on error.
-set -e
+# Make sure we have GNU make
+. "$ORIGIN_DIR/detect_gnu_make.sh"
+#gnu_make="$(command -v make 2>/dev/null)"
+#if command -v gmake >/dev/null 2>&1; then
+#    gnu_make="$(command -v gmake)"
+#fi
+#if [ -z "$gnu_make" -o "$("$gnu_make" --version 2>/dev/null | head -n 1 | cut -c-3)" != "GNU" ]; then
+#    echo >&2 "Could not find GNU make in PATH"
+#    exit 1
+#fi
 
 # Help text.
 if [ "$1" = '-h' ]; then
@@ -20,7 +28,7 @@ fi
 # Optional cleanup if requested.
 if [ "$1" = 'clean' ]; then
     rm -fv "$ORIGIN_DIR/objcopy" "$ORIGIN_DIR/objdump" "$ORIGIN_DIR/ld" "$ORIGIN_DIR/as"
-    rm -rfv "$BINUTILS_DIR"
+    rm -rf "$BINUTILS_DIR"
     rm -fv "$ORIGIN_DIR/$BINUTILS_XZ"
     exit
 fi
@@ -43,7 +51,7 @@ if [ ! -x "$BINUTILS_DIR/binutils/objcopy" ]; then
                 --disable-plugins \
                 --disable-gprof \
                 --enable-compressed-debug-sections=none && \
-    gmake all-ld all-gas MAKEINFO=true
+    "$gnu_make" all-ld all-gas MAKEINFO=true
     cd "$ORIGIN_DIR"
 fi
 
