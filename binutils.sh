0777099d8b3df6b1694dd3876274144669973068
Vladimir Vrzić <vvrzic@gmail.com>
2022-06-08 12:53:49 +0200

    Remove commented out code

--- binutils.sh
+++ binutils.sh
@@ -8,14 +8,6 @@ ORIGIN_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 # Make sure we have GNU make
 . "$ORIGIN_DIR/detect_gnu_make.sh"
-#gnu_make="$(command -v make 2>/dev/null)"
-#if command -v gmake >/dev/null 2>&1; then
-#    gnu_make="$(command -v gmake)"
-#fi
-#if [ -z "$gnu_make" -o "$("$gnu_make" --version 2>/dev/null | head -n 1 | cut -c-3)" != "GNU" ]; then
-#    echo >&2 "Could not find GNU make in PATH"
-#    exit 1
-#fi
 
 # Help text.
 if [ "$1" = '-h' ]; then
